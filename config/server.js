c56fa18a34a7d596040fde35e0ec5456bd219e09
contimo <diwakar.1neclick@gmail.com>
2021-07-28 21:05:55 +0530

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'caa12e7edf208b8dd00e35ad27361f67'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'a522d5ea-af0d-48f1-a422-5194d0f5b348'
+};
